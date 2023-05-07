529dd92109f9775f1fabb28155a9f0187767d0e2
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2023-05-07 10:23:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
-const fs = require('fs')
-const {
-    bot_token,
-    mongo_url
-} = JSON.parse(fs.readFileSync('./config.json'))
-const bot = new Telegraf(bot_token);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
